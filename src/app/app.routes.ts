d4d25c5d2cc23c9673f5c238abfd73ed0c804364
kusura-tech <litetsabr@gmail.com>
2026-07-22 12:16:02 +0300

    feat: initialize project architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+            },
+            {
+                path: 'about',
+                loadComponent: () => import('./pages/about/about.component').then(m => m.AboutComponent)
+            },
+            {
+                path: 'contacts',
+                loadComponent: () => import('./pages/contacts/contacts.component').then(m => m.ContactsComponent)
+            },
+            {
+                path: 'services',
+                loadComponent: () => import('./pages/services/services.component').then(m => m.ServicesComponent)
+            },
+            {
+                path: 'projects',
+                loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent)
+            },
+            {
+                path: 'news',
+                loadComponent: () => import('./pages/news/news.component').then(m => m.NewsComponent)
+            }
+        ]
+    },
+    {
+      path: 'admin',
+      loadChildren: () => import('./admin/admin.routes').then(m => m.ADMIN_ROUTES)
+    },
+];
