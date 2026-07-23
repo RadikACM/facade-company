509c02fe8ea1564117918d2fa921038cd7e261e4
kusura-tech <litetsabr@gmail.com>
2026-07-23 15:11:27 +0300

    feat: implement service for news

---
+++ src/app/core/services/news.service.ts
@@ -0,0 +1,48 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { Post } from '../models/news.model';
+import { addDoc, collection, collectionData, deleteDoc, doc, Firestore, updateDoc } from '@angular/fire/firestore';
+import { map, Observable, shareReplay } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  private firestore = inject(Firestore);
+  public isUploading = signal(false);
+  private postsRef = collection(this.firestore, 'news');
+  private postsData$: Observable<Post[]> = (
+    collectionData(this.postsRef, { idField: 'id' }) as Observable<any[]>
+  ).pipe(
+    map((posts) =>
+      posts.map((post) => ({
+        ...post,
+        create:
+          post.createdAt instanceof Date
+            ? post.createdAt
+            : new Date(post.createdAt),
+      })),
+    ),
+    shareReplay({ bufferSize: 1, refCount: true }),
+  );
+
+  getsPosts(): Observable<Post[]> {
+    return this.postsData$;
+  }
+
+  async addPost(post: Omit<Post, 'id' | 'createdAt'>) {
+    return await addDoc(this.postsRef, {
+      ...post,
+      createdAt: new Date(),
+    });
+  }
+
+  updatePost(postId: string, updatedPost: any) {
+    const docRef = doc(this.firestore, `news/${postId}`);
+    return updateDoc(docRef, updatedPost);
+  }
+
+  deletePost(postId: string) {
+    const docRef = doc(this.firestore, `news/${postId}`);
+    return deleteDoc(docRef);
+  }
+}
--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NewsService } from '../../core/services/news.service';
+import { Post } from '../../core/models/news.model';
 
 @Component({
   selector: 'app-news',
@@ -8,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class NewsComponent {
 
+  private readonly newsService = inject(NewsService);
+
+  addPost(): void {
+    const newPost: Omit<Post, 'id'> = {
+      title: 'New Post Title',
+      excerpt: 'This is a short excerpt of the new post.',
+      content: 'This is the full content of the new post.',
+      publishedAt: new Date().toISOString(),
+    };
+    this.newsService.addPost(newPost);
+  }
+  
 }
