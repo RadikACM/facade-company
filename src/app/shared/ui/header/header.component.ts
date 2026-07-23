791179d2468dadac36b080beeaa4a16a6a9e2962
Umar210696 <umarmansurovich@gmail.com>
2026-07-23 22:01:02 +0300

    feat: add header

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu () {
+   this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu () {
+    this.isMenuOpen = false;
+  }
 }
