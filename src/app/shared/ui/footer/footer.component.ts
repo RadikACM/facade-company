4d9aba9a3fbf2378395cf99b58e209d62227837d
Umar210696 <umarmansurovich@gmail.com>
2026-07-28 18:09:14 +0300

    feat: home-hero

--- src/app/shared/ui/footer/footer.component.ts
+++ src/app/shared/ui/footer/footer.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { ContactService } from '../../../core/services/contact.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  contactService = inject(ContactService);
+
+  contacts$ = this.contactService.getContacts()
 
 }
