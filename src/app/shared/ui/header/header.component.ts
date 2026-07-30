75789591804c3d5c2acf2c631cc5a338e83811e3
kusura-tech <litetsabr@gmail.com>
2026-07-30 15:02:38 +0300

    feat: header adding overlay

--- src/app/shared/ui/header/header.component.ts
+++ src/app/shared/ui/header/header.component.ts
@@ -1,24 +1,34 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   private readonly authService = inject(AuthService);
 
-  readonly isAdmin = this.authService.isAdmin
+  readonly isAdmin = this.authService.isAdmin;
   isMenuOpen = signal<boolean>(false);
 
   toggleMenu() {
-    this.isMenuOpen.update(isOpen => !isOpen);
+    this.isMenuOpen.update((prev) => !prev);
+    this.updateBodyScroll();
   }
 
   closeMenu() {
     this.isMenuOpen.set(false);
+    this.updateBodyScroll();
+  }
+
+  private updateBodyScroll() {
+    if (this.isMenuOpen()) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
   }
 }
